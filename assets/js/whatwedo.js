0cfd170833c7a32302feda37a3889c4e1eb6beaf
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2022-06-15 22:33:46 +0545

    animation added

--- assets/js/whatwedo.js
+++ assets/js/whatwedo.js
@@ -1,7 +1,22 @@
 $(window).on("load", function () {
+  // scroll product container
+  $(".scroll-wrapper").on("scroll", function () {
+    const currentPosition = $(this).scrollTop();
+    if (currentPosition === 0) {
+      $(".scroll-product-logo").removeClass("active");
+      $("#firstScrollProductLogo").addClass("active");
+    } else {
+      $(".scroll-product-logo").removeClass("active");
+      $("#secondScrollProductLogo").addClass("active");
+    }
+  });
+
   //   mobile product container
   $(".mobile-product-logo").on("click", function (e) {
     const currentId = $(this).attr("data-active");
+
+    $(".mobile-product-logo").removeClass("active");
+    $(this).addClass("active");
 
     $(".mobile-product-info").hide();
     $(`#${currentId}`).fadeIn();
